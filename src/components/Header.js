e299db019efba879c9bc0b3bf716e91b8edc7e29
Zara8170 <wlsgusan123@gmail.com>
2025-01-02 15:51:50 +0900

    로그인 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,62 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "../css/header.module.css";
 import { NavLink } from "react-router-dom";
+import Login from "./Login";
 
 export default function Header() {
+  const [showLogin, setShowLogin] = useState(false);
+
+  const handleLoginToggle = () => {
+    setShowLogin(!showLogin);
+  };
+
   return (
-    <div className={style.header_container}>
-      <NavLink to="/" className={style.logo}>
-        <img src="/logo.png" alt="Logo" />
-      </NavLink>
-      <div className={style.nav_container}>
-        <NavLink
-          to="/fashion&beauty"
-          className={({ isActive }) =>
-            isActive ? `${style.navbtn} ${style.active}` : style.navbtn
-          }
-        >
-          Fashion&Beauty
+    <>
+      <div className={style.header_container}>
+        <NavLink to="/" className={style.logo}>
+          <img src="/logo.png" alt="Logo" />
         </NavLink>
-        <NavLink
-          to="/f&b"
-          className={({ isActive }) =>
-            isActive
-              ? `${style.navshortbtn} ${style.active}`
-              : style.navshortbtn
-          }
-        >
-          F&B
-        </NavLink>
-        <NavLink
-          to="/health"
-          className={({ isActive }) =>
-            isActive
-              ? `${style.navshortbtn} ${style.active}`
-              : style.navshortbtn
-          }
-        >
-          Health
-        </NavLink>
-        <NavLink
-          to="/entertainment"
-          className={({ isActive }) =>
-            isActive ? `${style.navbtn} ${style.active}` : style.navbtn
-          }
-        >
-          Entertainment
-        </NavLink>
+        <div className={style.nav_container}>
+          <NavLink
+            to="/fashion&beauty"
+            className={({ isActive }) =>
+              isActive ? `${style.navbtn} ${style.active}` : style.navbtn
+            }
+          >
+            Fashion&Beauty
+          </NavLink>
+          <NavLink
+            to="/f&b"
+            className={({ isActive }) =>
+              isActive
+                ? `${style.navshortbtn} ${style.active}`
+                : style.navshortbtn
+            }
+          >
+            F&B
+          </NavLink>
+          <NavLink
+            to="/health"
+            className={({ isActive }) =>
+              isActive
+                ? `${style.navshortbtn} ${style.active}`
+                : style.navshortbtn
+            }
+          >
+            Health
+          </NavLink>
+          <NavLink
+            to="/entertainment"
+            className={({ isActive }) =>
+              isActive ? `${style.navbtn} ${style.active}` : style.navbtn
+            }
+          >
+            Entertainment
+          </NavLink>
+        </div>
+        <div className={style.login_container}>
+          <div className={style.loginbtn} onClick={handleLoginToggle}>
+            LOGIN
+          </div>
+          <NavLink
+            to="/signup"
+            className={({ isActive }) =>
+              isActive ? `${style.signbtn} ${style.active}` : style.signbtn
+            }
+          >
+            SIGNUP
+          </NavLink>
+        </div>
       </div>
-      <div className={style.login_container}>
-        <div className={style.loginbtn}>LOGIN</div>
-        <NavLink
-          to="/signup"
-          className={({ isActive }) =>
-            isActive ? `${style.signbtn} ${style.active}` : style.signbtn
-          }
-        >
-          SIGNUP
-        </NavLink>
-      </div>
-    </div>
+      {showLogin && <Login onClose={handleLoginToggle} />}
+    </>
   );
 }
